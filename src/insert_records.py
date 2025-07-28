ce44577359da39348ddcba858d9337add7d621fe
Dhasharadharami Reddy Bommana <dasarath.reddy22@gmail.com>
2025-07-28 07:51:38 +0000

    Spin up Postgres and Insert Data

--- src/insert_records.py
+++ src/insert_records.py
@@ -1,3 +1,86 @@
+import psycopg2
 from api_requests import mock_fetch_data
 
-print(mock_fetch_data())
+def connect_to_db():
+    print("Connecting to the PostgreSQL database...")
+    try:
+        conn = psycopg2.connect(
+            host="localhost",
+            port=5000,
+            dbname="db",
+            user="db_user",
+            password="db_password"
+        )
+        print("Connected successfully")
+        return conn
+    except psycopg2.Error as e:
+        print(f"Database connection failed: {e}")
+        raise
+
+def create_table(conn):
+    print("Creating table if not exist...")
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE SCHEMA IF NOT EXISTS bronze;
+            CREATE TABLE IF NOT EXISTS bronze.raw_weather_data(
+                id SERIAL PRIMARY KEY,
+                city TEXT,
+                temperature FLOAT,
+                weather_description TEXT,
+                wind_speed FLOAT,
+                time TIMESTAMP,
+                inserted_at TIMESTAMP DEFAULT NOW(),
+                utc_offset TEXT
+            );
+        """)
+        conn.commit()
+        print("Table was created")
+
+    except psycopg2.Error as e:
+        print(f"Failed to create table, {e}")
+        raise
+
+def insert_record(conn, data):
+    print("Inserting weather data into database...")
+    try:
+        weather = data["current"]
+        location = data["location"]
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO bronze.raw_weather_data(
+                city,
+                temperature,
+                weather_description,
+                wind_speed,
+                time,
+                inserted_at,
+                utc_offset
+            ) VALUES (%s, %s, %s, %s, %s, NOW(), %s)
+        """,(
+                location['name'],
+                weather['temperature'],
+                weather['weather_descriptions'][0],
+                weather['wind_speed'],
+                location['localtime'],
+                location['utc_offset']
+            )
+        )
+        conn.commit()
+        print("Data successfully inserted")
+    except psycopg2.Error as e:
+        print(f"Error inserting data into bronze.raw_weather_data, {e}")
+        raise
+
+def main():
+    try:
+        data = mock_fetch_data()
+        conn = connect_to_db()
+        create_table(conn)
+        insert_record(conn, data)
+    except Exception as e:
+        print(f"An error occurred during execution, {e}")
+    finally:
+        if 'conn' in locals():
+            conn.close()
+            print("Database connection closed")
